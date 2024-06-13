dd17a7ef4ed4124a88feebbc8e7c9513efda980d
BryantXtech <bryantuchiha7@gmail.com>
2024-06-13 11:09:08 +0000

    Update Madara.js

--- commands/Madara.js
+++ commands/Madara.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
-france({ nomCom: "madara", reaction: "😈", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+france({ nomCom: "Sharingan", reaction: "😈", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝐁𝐎𝐓 𝐈𝐒 𝐎𝐍𝐋𝐈𝐍𝐄 \n\n ' + "║❒ 𝐓𝐘𝐏𝐄 𝐌𝐄𝐍𝐔 𝐓𝐎 𝐒𝐄𝐄 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒";
+    let z = '𝐒𝚮𝚫𝚪𝚰𝚴𝐆𝚫𝚴 𝚫𝐂𝚻𝚰𝛁𝚫𝚻𝚵𝐃 \n\n ' + "║❒ 𝐓𝐘𝐏𝐄 𝐌𝐄𝐍𝐔 𝐓𝐎 𝐒𝐄𝐄 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒";
     let d = '                                            ║❒ 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠 https://t.me/+H2CcYqSn6zFhZTM0';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/388c8f5c23b4e7662ebfb.mp4';
